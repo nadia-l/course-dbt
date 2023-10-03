9afa0d077307db188710bafda657acaba3ce90bc
Nadia Lymperi <k.nadialymperi@gmail.com>
2023-10-03 15:28:40 +0000

    corrections

--- greenery/models/marts/product/intermediate/int_events_prep.sql
+++ greenery/models/marts/product/intermediate/int_events_prep.sql
@@ -26,7 +26,6 @@ select
     orders.shipping_cost,
     orders.order_total,
     orders.tracking_id,
-    --coalesce(events.product_id, order_items.product_id) as product_id,
     products.product_name,
     products.product_price
 from {{ ref('stg_events')}} as events
